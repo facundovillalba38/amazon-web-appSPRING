2b672b04cc40234c6771080f5cf2044567433287
facuvillalba12 <56897232+facuvillalba12@users.noreply.github.com>
2020-07-10 19:07:24 -0300

    Inicio app

---
+++ src/main/java/com/amazon/amazonwebapp/AmazonWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.amazon.amazonwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonWebAppApplication.class, args);
+	}
+
+}
